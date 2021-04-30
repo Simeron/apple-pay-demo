f97bd717d3358cfa44092912d781dff7cca77275
Simeron Taak <simeron.taak@cancer.org.uk>
2021-04-30 16:10:07 +0100

    first commit

---
+++ routes/checkout.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+
+router.post('/', (req, res, next) => {
+  const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: 'xbk6ybtcjpftqk29',
+    publicKey: '7f9ppqx3hy65mhv7',
+    privateKey: 'b3178d56cc68c2c0bed9d730c08f8df0'
+  });
+
+  // Use the payment method nonce here
+  const nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  const newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, (error, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
